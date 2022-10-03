25bf23f85a9cd01ad84fafc6a890b9697815f00a
ivatavuk <ivo.vatavuk@fer.hr>
2022-10-03 12:27:15 +0200

    Battery in grid

--- webpage/scripts/battery_visualize.js
+++ webpage/scripts/battery_visualize.js
@@ -1,14 +1,12 @@
 // Battery_visualize
 var battery_current_img = 1;
 var battery_visualization_started = false;
-var current_battery_percent = 75.0;
+var current_battery_percent = 78.32;
 const battery_segment_num = 5;
 var intervalID = window.setInterval(batteryTimerCallback, 100);
 function batteryTimerCallback() {
-  console.log(current_battery_percent);
   if (current_battery_percent != 100 && current_battery_percent != 0)
   {
-    console.log("tu smo");
     battery_current_img = 5 - Math.floor(current_battery_percent / (100 / battery_segment_num));
   }
   else if(current_battery_percent == 100)
@@ -21,10 +19,18 @@ function batteryTimerCallback() {
   }
   try
   {
-    document.getElementById("battery-visualizer").style.backgroundImage="url('images/battery/battery_" + battery_current_img + ".png')";
+    document.getElementById("battery-visualizer-img").style.backgroundImage="url('images/battery/battery_" + battery_current_img + ".png')";
   }
   catch
   {
-    console.log("battery_visualize.js ERR => Failed to get battery image!");  
+    console.log("battery_visualize.js ERR => Failed to get battery visualizer img!");  
+  }
+  try
+  {
+    document.getElementById("battery-visualizer-text").innerHTML = Math.round(current_battery_percent) + "%";
+  }
+  catch
+  {
+    console.log("battery_visualize.js ERR => Failed to get battery visualizer text!");  
   }
 }
