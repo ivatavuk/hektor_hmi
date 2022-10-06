805bcb14f5e12f2e14136d17bfaf0405fe52e009
ivatavuk <ivo.vatavuk@fer.hr>
2022-10-06 14:28:39 +0200

    Add a toggle button for drive info printing

--- webpage/scripts/viv/drives_visualize_viv.js
+++ webpage/scripts/viv/drives_visualize_viv.js
@@ -2,6 +2,24 @@
 var current_velocities = [0, 0, 0, 0];
 
 var intervalID = window.setInterval(drivesTimerCallback, 100);
+
+drive_printing = true;
+
+function togglePauseDrivePrinting() {
+  drive_printing = !drive_printing;
+  try
+  {
+    if(drive_printing)
+      document.getElementById("toggle_pause_printing_button").innerHTML = "PAUSE PRINTING"; 
+    else
+      document.getElementById("toggle_pause_printing_button").innerHTML = "RESUME PRINTING";
+  }
+  catch
+  {
+    console.log("drives_visualize_viv.js ERR");  
+  }
+}
+
 function drivesTimerCallback() {
   if(!localStorage.getItem('viv')) {
     viv_status = {};
@@ -11,15 +29,17 @@ function drivesTimerCallback() {
   }
   
   current_velocities = viv_status.drive_data_vel;
-  try
-  {
-    document.getElementById("drive_1").innerHTML = Math.round(current_velocities[0] * 100) / 100 + " rad/s";
-    document.getElementById("drive_2").innerHTML = Math.round(current_velocities[1] * 100) / 100 + " rad/s";
-    document.getElementById("drive_3").innerHTML = Math.round(current_velocities[2] * 100) / 100 + " rad/s";
-    document.getElementById("drive_4").innerHTML = Math.round(current_velocities[3] * 100) / 100 + " rad/s";
-  }
-  catch
-  {
-    console.log("drives_visualize_viv.js ERR");  
+  if(drive_printing) {
+    try
+    {
+      document.getElementById("drive_1").innerHTML = Math.round(current_velocities[0] * 100) / 100 + " °/s";
+      document.getElementById("drive_2").innerHTML = Math.round(current_velocities[1] * 100) / 100 + " °/s";
+      document.getElementById("drive_3").innerHTML = Math.round(current_velocities[2] * 100) / 100 + " °/s";
+      document.getElementById("drive_4").innerHTML = Math.round(current_velocities[3] * 100) / 100 + " °/s";
+    }
+    catch
+    {
+      console.log("drives_visualize_viv.js ERR");  
+    }
   }
 }
