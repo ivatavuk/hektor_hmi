e7e2f95c8d6c10b1753d45916b7730f0b8d60fda
ivatavuk <ivo.vatavuk@fer.hr>
2022-10-07 13:37:51 +0200

    Update drive positions and add reduction

--- webpage/scripts/viv/drives_visualize_viv.js
+++ webpage/scripts/viv/drives_visualize_viv.js
@@ -27,15 +27,15 @@ function drivesTimerCallback() {
   else {
     viv_status = JSON.parse(localStorage.getItem('viv'));
   }
-  
+  reduction = 81
   current_velocities = viv_status.drive_data_vel;
   if(drive_printing) {
     try
     {
-      document.getElementById("drive_1").innerHTML = Math.round(current_velocities[0] * 100) / 100 + " °/s";
-      document.getElementById("drive_2").innerHTML = Math.round(current_velocities[1] * 100) / 100 + " °/s";
-      document.getElementById("drive_3").innerHTML = Math.round(current_velocities[2] * 100) / 100 + " °/s";
-      document.getElementById("drive_4").innerHTML = Math.round(current_velocities[3] * 100) / 100 + " °/s";
+      document.getElementById("drive_1").innerHTML = Math.round(current_velocities[0] / reduction * 100) / 100 + " °/s";
+      document.getElementById("drive_2").innerHTML = Math.round(current_velocities[1] / reduction * 100) / 100 + " °/s";
+      document.getElementById("drive_3").innerHTML = Math.round(current_velocities[2] / reduction * 100) / 100 + " °/s";
+      document.getElementById("drive_4").innerHTML = Math.round(current_velocities[3] / reduction * 100) / 100 + " °/s";
     }
     catch
     {
