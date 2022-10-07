81e35dd2471d7603fb48c947c338c1c855332a48
ivatavuk <ivo.vatavuk@fer.hr>
2022-10-07 16:33:00 +0200

    Add unit switching button (rad or deg)

--- webpage/scripts/viv/kinova_status_writing.js
+++ webpage/scripts/viv/kinova_status_writing.js
@@ -1,4 +1,26 @@
 var intervalID = window.setInterval(statusTimerCallback, 100);
+var units = "deg";
+
+function toggleUnits() {
+  try
+  {
+    if(units == "deg")
+    {
+      units = "rad";
+      document.getElementById("toggle_units_button").innerHTML = "SWITCH TO DEGREES"; 
+    }
+    else
+    {
+      units = "deg";
+      document.getElementById("toggle_units_button").innerHTML = "SWITCH TO RADIANS";
+    }
+      
+  }
+  catch
+  {
+    console.log("kinova_status_writing.js ERR");  
+  }
+}
 
 function statusTimerCallback() {
   if(!localStorage.getItem('viv')) {
@@ -12,14 +34,15 @@ function statusTimerCallback() {
   if(!joint_pos_list)
     joint_pos_list = [0, 0, 0, 0, 0, 0, 0];
     
-
   try
   {
     for(counter = 1; counter <= 7; counter++)
     {
-      document.getElementById("joint-pos-" + counter).innerHTML = Math.round(joint_pos_list[counter - 1] * 10) / 10 + " °";
+      if(units == "rad")
+        document.getElementById("joint-pos-" + counter).innerHTML = Math.round(joint_pos_list[counter - 1] * 10) / 10 + " rad";
+      if(units == "deg")
+        document.getElementById("joint-pos-" + counter).innerHTML = Math.round(joint_pos_list[counter - 1] * 180.0 / 3.14159 * 10) / 10 + " °";
     }
-      
   }
   catch
   {
